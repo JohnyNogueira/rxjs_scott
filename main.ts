724c0bc1e4024b41d42036fe155f95ae7831af22
Johny Nogueira <johny.n.c@hotmail.com>
2017-03-24 18:14:56 -0300

    work with observables and fetch

---
+++ main.ts
@@ -0,0 +1,79 @@
+// import { Observable, Observer } from "rxjs";
+
+import { Observable } from "rxjs";
+
+let output = document.getElementById("output");
+let button = document.getElementById("button");
+
+let click = Observable.fromEvent(button, "click")
+
+
+function load(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();        
+        xhr.addEventListener("load", () => {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+        });
+        xhr.open("GET", url);
+        xhr.send();
+    }).retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
+
+}
+
+function loadWithFetch(url: string) {
+    return Observable.defer(() => {
+        return Observable.fromPromise(fetch(url).then(r => r.json()));
+    });
+}
+
+function retryStrategy({ attempts = 4, delay = 1000 }) {
+    return function (errors) {
+        return errors
+            .scan((acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    }
+}
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+loadWithFetch("movies.json").subscribe(renderMovies);
+
+click.flatMap(e => loadWithFetch("movies.json"))
+    .subscribe(
+    renderMovies,
+    e => console.log(`error: ${e}`),
+    () => console.log('complete')
+    );
+
+// class MyObserver implements Observer<number>{
+//     next(value) {
+//         console.log(`value: ${value}`);
+//     }
+
+//     error(err) {
+//         console.log(`error: ${err}`);
+//     }
+
+//     complete(){
+//         console.log('complete');
+//     }
+// }
+
+// source.subscribe(new MyObserver());
+// source.subscribe(new MyObserver());
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    entry: "./main",
+    output: { filename: "app.js" },
+    module: {
+        loaders: [
+            {
+                test: /.ts$/,
+                loader: "ts-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    }
+}
